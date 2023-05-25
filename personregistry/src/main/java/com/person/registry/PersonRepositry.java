e69c7cdba6d59c619114b67c3992e3a864e5ab37
ReshuMahobiya <134595026+ReshuMahobiya@users.noreply.github.com>
2023-05-25 11:12:37 +0200

    Delete personregistry directory
    deleted Directory

--- personregistry/src/main/java/com/person/registry/PersonRepositry.java
+++
@@ -1,9 +0,0 @@
-package com.person.registry;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepositry extends JpaRepository<Person, Long>{
-
-}
